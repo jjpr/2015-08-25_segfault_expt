b92a9880fbda58024fedd12a0ea99e6b2ec538cc
Jonathan J. Prescott-Roy <jjprescottroy@gmail.com>
2015-08-25 17:45:46 -0400

    I don't fully get the ordering of the output yet.

--- scratch.py
+++ scratch.py
@@ -4,14 +4,16 @@ import os, signal
 from multiprocessing import Process
 
 def kill_me():
-  print("before")
+  print("before kill")
   os.kill(os.getpid(), signal.SIGSEGV)
-  print("after")
+  print("after kill")
 
 def start_process():
   p = Process(target=kill_me)
   p.start()
+  print("after start")
   p.join()
+  print("after join")
 
 if __name__ == "__main__":
   start_process()
