d80e96df01f8a6f922524b3fb0b7709ad6e8538d
Jonathan J. Prescott-Roy <jjprescottroy@gmail.com>
2015-08-26 10:14:48 -0400

    Using Queue to pass objects back.

--- scratch.py
+++ scratch.py
@@ -1,19 +1,25 @@
 __author__ = 'jjpr'
 
 import os, signal
-from multiprocessing import Process
+from multiprocessing import Process, Queue
 
-def kill_me():
+def kill_me(q):
+  some_stuff = ("foo", 42.0)
+  q.put(some_stuff)
   print("before kill")
   os.kill(os.getpid(), signal.SIGSEGV)
   print("after kill")
 
 def start_process():
-  p = Process(target=kill_me)
+  q = Queue()
+  p = Process(target=kill_me, args=(q,))
   p.start()
-  print("after start")
+  # print("after start")
+  stuff = q.get()
+  print("after get")
   p.join()
   print("after join")
+  print(stuff)
 
 if __name__ == "__main__":
   start_process()
